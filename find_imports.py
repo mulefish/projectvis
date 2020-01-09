15295e252e505bdfc6b44f94314c7d206173a7df
Paul Montgomery <paul.montgomery@PRFT-11395-MAC.domain>
2020-01-09 10:55:52 -0600

    about to start in on imports counting

--- find_imports.py
+++ find_imports.py
@@ -1,16 +1,32 @@
-p = "data1/react-redux-realworld-example-app-master/src/components/ListPagination.js"
 
 def readFileForImports(path):
+    results = []
     with open(path) as fp:
         line = fp.readline()
-        cnt = 1
         while line:
             if "import" in line and "from" in line:
-                print("Line {}: {}".format(cnt, line.strip()))
-
-            line = fp.readline()
+                result = line.strip()
+                # import { SET_PAGE } from '../constants/actionTypes';
+                result = result.split("from")[1]
                 
-            cnt += 1
+                # '../constants/actionTypes';
+                result = result.replace(";","")
+                result = result.replace("'","")
+                result = result.strip()
+
+                # ../constants/actionTypes
+                results.append(result)
 
 
-readFileForImports(p)
+
+                # print("Line {}: {}".format(line.strip()))
+            line = fp.readline()
+    return results
+
+if __name__ == "__main__":
+    """Self test"""
+    p = "data1/react-redux-realworld-example-app-master/src/components/ListPagination.js"
+    results = readFileForImports(p)
+    for item in results:
+        print(item)
+
--- walk.py
+++ walk.py
@@ -2,14 +2,17 @@ import os
 import sys
 from letter import IDService
 
-class FileNode:
-    """ This will hold a file name. It might or might not appear in different _trees_. """
 
-    def __init__(self, full_path, letter, filename):
+
+class FileNode:
+    """ This will hold a file name. It might or might also not appear in different _trees_. """
+
+    def __init__(self, full_path, letter, filename, short_path):
         self.paths = []
         self.paths.append(full_path)        
         self.letter = letter
         self.filename = filename
+        self.short_path = short_path
 
     def addAnotherFullPath(self, full_path):
         if full_path not in self.paths:
@@ -17,14 +20,20 @@ class FileNode:
 
     def display(self):
         n = len(self.paths)
-        print("{} {} {}".format(n, self.letter, self.filename))
+        print("{} {} {}".format(n, self.letter, self.short_path))
         for item in self.paths:
             print("\t|{}|".format( item ))
 
 
 walk_dirs = []
-walk_dirs.append("data1") # sys.argv[1] # Compare this 
-walk_dirs.append("data2") # sys.argv[1] # to this
+# walk_dirs.append("data1") # sys.argv[1] # Compare this 
+# walk_dirs.append("data2") # sys.argv[1] # to this
+
+walk_dirs.append("data1/react-redux-realworld-example-app-master/src") # sys.argv[1] # Compare this 
+walk_dirs.append("data2/react-redux-realworld-example-app-master/src") # sys.argv[1] # Compare this 
+
+
+
 possible = {}
 
 def step1_read_dirs(ary):
@@ -34,20 +43,23 @@ def step1_read_dirs(ary):
 
 
         for root, subdirs, files in os.walk(walk_dir):
-            #print("{}    {}".format( walk_dir, root ))
+            print("{}    {}".format( walk_dir, root ))
             for subdir in subdirs:
                 k = 0 
                 for filename in files:
                     if filename.endswith(".jsx") or filename.endswith(".js"):
                         full_path = os.path.join(root, filename)
                         short_path = full_path
-                        short_path = short_path.replace(walk_dir + "/",  "|")
+                        # short_path = short_path.replace(walk_dir + "/",  "|")
+                        short_path = full_path.split("src/")[1]
+
+                        
                         if short_path in possible:
                             # Seen this file before! But in a different tree
                             possible[short_path].addAnotherFullPath(full_path)
                         else:
                             letter = id.label_gen()
-                            fn = FileNode(full_path, letter, filename)
+                            fn = FileNode(full_path, letter, filename, short_path)
                             possible[short_path] = fn
 
 step1_read_dirs(walk_dirs)
