7cb92dc1a051ce3426674b35ea15a5942b994e4a
Paul Montgomery <paul.montgomery@PRFT-11395-MAC.domain>
2020-01-09 13:47:22 -0600

    Fixed a only semi-correct recursive decent

--- find_imports.py
+++ find_imports.py
@@ -1,32 +1,37 @@
+import json
 
-def readFileForImports(path):
-    results = []
-    with open(path) as fp:
-        line = fp.readline()
-        while line:
-            if "import" in line and "from" in line:
-                result = line.strip()
-                # import { SET_PAGE } from '../constants/actionTypes';
-                result = result.split("from")[1]
-                
-                # '../constants/actionTypes';
-                result = result.replace(";","")
-                result = result.replace("'","")
-                result = result.strip()
-
-                # ../constants/actionTypes
-                results.append(result)
-
-
-
-                # print("Line {}: {}".format(line.strip()))
+class ReadFileForImports:
+    
+    def readFileForImports(self, path):
+        results = []
+        with open(path) as fp:
             line = fp.readline()
-    return results
+            while line:
+                if "import" in line and "from" in line:
+                    result = line.strip()
+                    # import { SET_PAGE } from '../constants/actionTypes';
+                    result = result.split("from")[1]
+                
+                    # '../constants/actionTypes';
+                    result = result.replace(";","")
+                    result = result.replace("'","")
+                    result = result.strip()
+
+                    # ../constants/actionTypes
+                    results.append(result)
+
+                line = fp.readline()
+        return results
 
 if __name__ == "__main__":
     """Self test"""
     p = "data1/react-redux-realworld-example-app-master/src/components/ListPagination.js"
-    results = readFileForImports(p)
-    for item in results:
-        print(item)
-
+    rffi = ReadFileForImports()
+    results = rffi.readFileForImports(p)
+    actual = json.dumps(results)
+    expected = json.dumps(["react","../agent","react-redux","../constants/actionTypes"])
+    
+    if expected == actual:
+        print("PASS find_imports.py")
+    else:
+        print("FAIL find_imports.py")
--- walk.py
+++ walk.py
@@ -1,70 +1,89 @@
 import os
 import sys
 from letter import IDService
+from find_imports import ReadFileForImports
+
 
 
 
 class FileNode:
     """ This will hold a file name. It might or might also not appear in different _trees_. """
 
-    def __init__(self, full_path, letter, filename, short_path):
+    def __init__(self, short_name, file, full_name, letter):
         self.paths = []
-        self.paths.append(full_path)        
-        self.letter = letter
-        self.filename = filename
-        self.short_path = short_path
-
-    def addAnotherFullPath(self, full_path):
-        if full_path not in self.paths:
-            self.paths.append(full_path)
+        self.short_name = short_name
+        self.file = file
+        self.letter = letter 
+        #self.paths.append(short_name)        
+        self.paths.append(full_name)        
+        
+    def addNode(self, short_name, full_name):
+        #self.paths.append(short_name)
+        self.paths.append(full_name)
 
     def display(self):
-        n = len(self.paths)
-        print("{} {} {}".format(n, self.letter, self.short_path))
-        for item in self.paths:
-            print("\t|{}|".format( item ))
+        for full_name in self.paths:
+            print("\tID {}   path {} for {}" .format(self.letter, full_name, self.short_name ))
 
 
-walk_dirs = []
-# walk_dirs.append("data1") # sys.argv[1] # Compare this 
-# walk_dirs.append("data2") # sys.argv[1] # to this
+def buildShortName(path, filename, delimiter):
+    """ 
+    CONVERT 
+    ['.', 'data2', 'src', 'components', 'Article'] CommentContainer.js 
+    TO
+    components/Article/CommentContainer.js
+    """    
+    result = ""
+    flag = False
+    for dir in path:
+        if flag == True:
+            result += dir + "/"
+        if dir == delimiter:
+            flag = True
+    result += filename
+    return result
 
-walk_dirs.append("data1/react-redux-realworld-example-app-master/src") # sys.argv[1] # Compare this 
-walk_dirs.append("data2/react-redux-realworld-example-app-master/src") # sys.argv[1] # Compare this 
+def buildFullName(path, filename):
+    """ 
+    CONVERT 
+    ['.', 'data2', 'src', 'components', 'Article'] CommentContainer.js 
+    TO
+    ./data2/src/components/Article/CommentContainer.js
+    """    
+
+    result = ""
+    for dir in path:
+        result += dir + "/"
+    result += filename
+    return result
 
 
 
-possible = {}
 
-def step1_read_dirs(ary):
+possible = {} 
+def step1_read_dirs(dirs):
     id = IDService()
+    for dir in dirs:
+        for root, dirs, files in os.walk(dir):
+            path = root.split(os.sep)
+            for file in files:
+                if file.endswith(".js") or file.endswith(".jsx"):        
+                    short_name = buildShortName(path, file, "src")
+                    full_name = buildFullName(path, file)
+                    if short_name in possible:
+                        possible[short_name].addNode(short_name, full_name)
+                    else:
+                        letter = id.label_gen()
+                        node = FileNode(short_name, file, full_name, letter)
+                        possible[short_name] = node
 
-    for walk_dir in ary:
+starting_points = [] 
+starting_points.append("./data1/")
+starting_points.append("./data2/")
+step1_read_dirs(starting_points)
 
 
-        for root, subdirs, files in os.walk(walk_dir):
-            print("{}    {}".format( walk_dir, root ))
-            for subdir in subdirs:
-                k = 0 
-                for filename in files:
-                    if filename.endswith(".jsx") or filename.endswith(".js"):
-                        full_path = os.path.join(root, filename)
-                        short_path = full_path
-                        # short_path = short_path.replace(walk_dir + "/",  "|")
-                        short_path = full_path.split("src/")[1]
 
-                        
-                        if short_path in possible:
-                            # Seen this file before! But in a different tree
-                            possible[short_path].addAnotherFullPath(full_path)
-                        else:
-                            letter = id.label_gen()
-                            fn = FileNode(full_path, letter, filename, short_path)
-                            possible[short_path] = fn
-
-step1_read_dirs(walk_dirs)
 for key in possible:
-    possible[key].display()
-
-
-
+    x = possible[key]
+    x.display()
