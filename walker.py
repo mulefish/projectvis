2cc74b876ad9097b0f7d393f9bc9c180165b94e1
Paul Montgomery <paul.montgomery@PRFT-11395-MAC.domain>
2020-01-23 22:12:23 -0600

    cleanup

--- walker.py
+++ walker.py
@@ -4,19 +4,19 @@ from letter import IDService
 from find_imports import ReadFileForImports
 import json
 
-"""
-1: Create a dataviz of a  well-formed REACT project. 
-2: Nodes will be files inside the project. Each node will import zero or more other nodes from
-inside this project. This will ignore imports from  outside of the project. These imported refs will be 
-the edges. 
-3: This will also be able to import 2 different versions. I am thinking branch1 vs. branch2 of a project. 
-"""
+class Virtual:
+    def __init__(self, fictionalAbs ):
+        self.fictionalAbs = fictionalAbs # e.g., 'components/reducers/Logic.js'
+
+
 
 
 possible = {}
 nodes = {} 
 idService = IDService()
-
+SEP = os.sep 
+THIS_DIR = "." + SEP
+UP_DIR = ".." + SEP
 class Node:
     def __init__(self, branch, path_to_file, letter, depth):
         self.refs = {}
@@ -24,49 +24,14 @@ class Node:
         self.letter = letter
         self.depth = depth # a/b/c/something.js = 3 but a/b/c/d/e/f/something-else.js = 6  
         
-    def addRefsForThisBranch(self, branch, refArray):
-        ary = [] 
-        b = branch.replace("./", "")
-        for ref in refArray:
-            x = ref.replace(b, "")
-            ary.append(x)
-        self.refs[branch] = ary
-
-    def display(self):
-        print("{}  DEPTH={} FILE={} ".format(self.letter, self.depth,self.path_to_file ))
-        for branch in self.refs:
-            for ref in self.refs[branch]:
-                letter = nodes[ref].letter
-                print("{}\t\t{}".format(letter, ref))
-
 def clean_the_path(path_to_file, branch):
     # get ./data1/src/components/Profile.js
     # return data1/src/components/Profile
-    path_to_file = path_to_file.replace(branch, "")
-    path_to_file = path_to_file.replace(".jsx", "")
-    path_to_file = path_to_file.replace(".js", "")
-    path_to_file = path_to_file.replace("./", "")
+    # path_to_file = path_to_file.replace(branch, "")
+    # path_to_file = path_to_file.replace(".jsx", "")
+    # path_to_file = path_to_file.replace(".js", "")
+    # path_to_file = path_to_file.replace(THIS_DIR, "")
     return path_to_file
-
-def findAbsPath(path_to_file, relative_path):
-    #pathToFile = "a/b/c/d"
-    #relative_path = "../../something"
-    #expected = "a/b/something"
-    count = relative_path.count("..", 0, len(relative_path))
-    path = path_to_file.split(os.sep)
-    sep = "/"
-    #absolute = sep.join(path[:count]) # <--- I wanted this to work: The below seems clunky.
-    n = len(path)
-    n -= count
-    absolute = ""
-    for d in range(n):
-        absolute += path[d] + sep
-    
-    absolute += relative_path.replace("../","")
-    absolute = absolute.replace("//","/")
-    absolute = absolute.replace("./","")
-
-    return absolute
 
 def step1_populate_possible_refs(dirs):
     # Populate a hash of possible refs. If there is a ref made and 
@@ -75,67 +40,68 @@ def step1_populate_possible_refs(dirs):
     for branch in dirs:
         for root, dirs, files in os.walk(branch):
             for file in files:
-                if file.endswith(".js") or file.endswith(".jsx"):   
-                    file = clean_the_path(file, branch)     
-                    
-                    if file in possible:
-                        possible[file] += 1
+
+                absFile = "{}{}{}".format(root, SEP, file)
+                if absFile.endswith(".js") or absFile.endswith(".jsx"):   
+                    absFile = clean_the_path(absFile, branch)     
+                    if absFile in possible:
+                        possible[absFile] += 1
                     else:
-                        possible[file] = 1
+                        possible[absFile] = 1
 
-rffi = ReadFileForImports()
-def step2_read_dirs(dirs):
-    for branch in dirs:
-        for root, dirs, files in os.walk(branch):
-            path = root.split(os.sep)
-            # print("...")
-            for file in files:
-                if file.endswith(".js") or file.endswith(".jsx"):        
-                    sep = "/"
-                    path_to_file = sep.join(path) 
-                    path_to_file += sep + file
-                    imports = rffi.readFileForImports(path_to_file)
-                    cleaned = clean_the_path(path_to_file, branch)
-                    if cleaned not in nodes:
-                        letter = idService.label_gen()
-                        node = Node(branch, cleaned, letter, len(path))
-                        nodes[cleaned] = node
-                    good = []
-                    bad = []
-                    for importedRef in imports:
-                        path_to_ref = findAbsPath(root, importedRef)
-                        ref = path_to_ref.split(os.sep)[-1]
-                        if ref in possible:
-                            good.append(path_to_ref)
-                        else:
-                            bad.append(path_to_ref)
-                    nodes[cleaned].addRefsForThisBranch(branch, good)                    
+# rffi = ReadFileForImports()
+# def step2_read_dirs(dirs):
+#     for branch in dirs:
+#         for root, dirs, files in os.walk(branch):
+#             path = root.split(os.sep)
+#             # print("...")
+#             for file in files:
+#                 if file.endswith(".js") or file.endswith(".jsx"):        
+#                     sep = "/"
+#                     path_to_file = sep.join(path) 
+#                     path_to_file += sep + file
+#                     imports = rffi.readFileForImports(path_to_file)
+#                     cleaned = clean_the_path(path_to_file, branch)
+#                     if cleaned not in nodes:
+#                         letter = idService.label_gen()
+#                         node = Node(branch, cleaned, letter, len(path))
+#                         nodes[cleaned] = node
+#                     good = []
+#                     bad = []
+#                     for importedRef in imports:
+#                         path_to_ref = findAbsPath(root, importedRef)
+#                         ref = path_to_ref.split(os.sep)[-1]
+#                         if ref in possible:
+#                             good.append(path_to_ref)
+#                         else:
+#                             bad.append(path_to_ref)
+#                     nodes[cleaned].addRefsForThisBranch(branch, good)                    
 
-def show3_show():
-    for key in nodes:
-        node = nodes[key]
-        node.display()
+# def show3_show():
+#     for key in nodes:
+#         node = nodes[key]
+#         node.display()
 
-def step3_emit_to_file_as_json():
-    info = {}
-    for key in nodes:
-        node = nodes[key]
-        letter = node.letter
-        ref = node.refs
-        depth = node.depth
+# def step3_emit_to_file_as_json():
+#     info = {}
+#     for key in nodes:
+#         node = nodes[key]
+#         letter = node.letter
+#         ref = node.refs
+#         depth = node.depth
 
-        info[key] = {}
-        info[key]['letter'] = letter
-        info[key]['ref'] = ref
-        info[key]['depth'] = depth
-    file='data.js' 
-    with open(file, 'w') as filetowrite:
-        filetowrite.write("const data = {}".format(json.dumps(info)))
+#         info[key] = {}
+#         info[key]['letter'] = letter
+#         info[key]['ref'] = ref
+#         info[key]['depth'] = depth
+#     file='data.js' 
+#     with open(file, 'w') as filetowrite:
+#         filetowrite.write("const data = {}".format(json.dumps(info)))
 
-    # print( json.dumps(info))
-    for x in info:
-        print(x)
-    print("\n*** Wrote to {} ***\n".format(file))
+#     # print( json.dumps(info))
+#     for x in info:
+#         print(x)
+#     print("\n*** Wrote to {} ***\n".format(file))
 
 
 if __name__ == "__main__":
@@ -143,6 +109,11 @@ if __name__ == "__main__":
     starting_points.append("./data1/")
     starting_points.append("./data2/")
     step1_populate_possible_refs(starting_points)
-    step2_read_dirs(starting_points)
-    # show3_show()
-    step3_emit_to_file_as_json()
+    # step2_read_dirs(starting_points)
+    # # show3_show()
+    # step3_emit_to_file_as_json()
+
+    for key in possible:
+        value = possible[key]
+        print( "{}   {} ".format(value, key ))
+        
